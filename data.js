b31c28738b25898f233f97bb03b4a2b6eb6fb041
Iryna <ielkinairinka@gmail.com>
2022-06-10 18:38:37 +0300

    *

--- data.js
+++ data.js
@@ -3,19 +3,18 @@
 
 //месяцы считаются с нуля 
 //учитывает часовые пояса 
-// const now = new Date('2022-06-10');
-// // new Date.parse('2022-06-10');
-// console.log(now.setHours());
-// console.log(now);
-
-// console.log(now.getFullYear()); //текущий год
-// console.log(now.getMonth());
-// console.log(now.getDate());
-// console.log(now.getDay());
-// console.log(now.getHours());
-// console.log(now.getUTCHours());
-// console.log(now.getTimezoneOffset());
-// console.log(now.getTime());
+const now = new Date('2022-06-10');
+// new Date.parse('2022-06-10');
+console.log(now.setHours());
+console.log(now);
+console.log(now.getFullYear()); //текущий год
+console.log(now.getMonth());
+console.log(now.getDate());
+console.log(now.getDay());
+console.log(now.getHours());
+console.log(now.getUTCHours());
+console.log(now.getTimezoneOffset());
+console.log(now.getTime());
 
 let start = new Date();
 for (let i = 0; i < 100000; i++) {
