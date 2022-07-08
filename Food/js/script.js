4794b332a029f158bbb9d4bdbe00ff2766880618
Ирина <ielkinairinka@gmail.com>
2022-07-08 21:31:45 +0300

    048 Используем классы в реальной работе

--- Food/js/script.js
+++ Food/js/script.js
@@ -119,7 +119,7 @@ window.addEventListener("DOMContentLoaded", () => {
     }
   });
   //вызов модального окна через определенное время
-  const modalTimerId = setTimeout(openModal, 5000);
+  // const modalTimerId = setTimeout(openModal, 5000);
   //вызов модального окна через прокрутку страницы
   function showModalByScroll() {
     if (window.pageYoffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
@@ -128,4 +128,72 @@ window.addEventListener("DOMContentLoaded", () => {
     }
   }
   window.addEventListener('scroll', showModalByScroll);
+  //Использование классов для карточек
+  class MenuCard {
+    constructor(src, alt, title, descr, price, parentSelector) {
+      this.src = src;
+      this.alt = alt;
+      this.title = title;
+      this.descr = descr;
+      this.price = price;
+      this.parent = document.querySelector(parentSelector);
+      this.transfer = 27;
+      this.changeToUAN();
+    }
+    changeToUAN() {
+      this.price = +this.price * this.transfer;
+
+
+    }
+    render() {
+      const element = document.createElement('div');
+      element.innerHTML = `
+        <div class="menu__item">
+          <img src=${this.src} alt=${this.alt} />
+          <h3 class="menu__item-subtitle">${this.title}</h3>
+          <div class="menu__item-descr">
+          ${this.descr}
+          </div>
+          <div class="menu__item-divider"></div>
+          <div class="menu__item-price">
+            <div class="menu__item-cost">Цена:</div>
+            <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+          </div>
+        </div>
+      `;
+      this.parent.append(element);
+    }
+  }
+  // const div = new MenuCard();
+  // div.render();
+  //или
+  new MenuCard(
+    "img/tabs/vegy.jpg",
+    "vegy",
+    'Меню "Фитнес"',
+    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов.Продукт активных и здоровых людей.Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+    9,
+    '.menu .container'
+
+  ).render();
+
+  new MenuCard(
+    "img/tabs/vegy.jpg",
+    "vegy",
+    'Меню "Фитнес"',
+    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов.Продукт активных и здоровых людей.Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+    9,
+    '.menu .container'
+
+  ).render();
+
+  new MenuCard(
+    "img/tabs/vegy.jpg",
+    "vegy",
+    'Меню "Фитнес"',
+    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов.Продукт активных и здоровых людей.Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+    9,
+    '.menu .container'
+  ).render();
+
 });
